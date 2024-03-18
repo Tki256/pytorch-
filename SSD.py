446659ad53dce97198e0c2bf7b509c85a4dd9e0e
Taiki Mizutani <tki256@Tkipc>
2024-03-19 03:20:21 +0900

    SSD ネットワークモデル

--- SSD.py
+++ SSD.py
@@ -266,7 +266,7 @@ class VOCDataset(data.Dataset):
         self.transform = transform
         self.transform_anno = transform_anno
         
-    def __len(self):
+    def __len__(self):
         return len(self.img_list)
     
     def __getitem__(self, index):
@@ -314,3 +314,164 @@ val_dataset = VOCDataset(val_img_list, val_anno_list, phase="val",
 # データの取り出し例
 val_dataset.__getitem__(1)
         
+# %% DataLoaderの作成
+def od_collate_fn(batch):
+    """
+    Datasetから取り出すアノテーションデータのサイズが画像ごとに異なる
+    画像内の物体数が2個であれば(2,5)であるが3庫であれば(3,5)などに変化する
+    この変化に対応したDataLoaderを作成するためカスタマイズしたcollate_fnを作成
+    collate_fnはPytorchでリストからmini-batchを作成する関数
+    ミニバッチ分の画像が並んでいるリスト変数batchに，ミニバッチ番号を指定する次元を先頭に1つ追加してリストの形を変形
+    """
+    
+    targets = []
+    imgs = []
+    for sample in batch:
+        imgs.append(sample[0])
+        targets.append(torch.FloatTensor(sample[1])) # sample[1]はアノテーションgt
+        
+    # imgsはミニバッチサイズのリスト
+    # リストの要素はtorch.Size([3,300,300])
+    # このリストをtorch.Size([batch_num, 3, 300, 300])のテンソルに変換
+    imgs = torch.stack(imgs, dim=0)
+    
+    # targetsはアノテーションデータの正解であるgtのリスト
+    # リストのサイズはミニバッチサイズ
+    # リストtargetsの要素は[n, 5]となる
+    # nは画像ごとに異なり，画像内にある物体の数となる
+    # 5は[xmin, ymin, xmax, tmax, class_index]
+    
+    return imgs, targets
+
+
+# %% データローダーの作成
+
+batch_size = 4
+
+train_dataloader = data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True, collate_fn=od_collate_fn
+)
+
+val_dataloader = data.DataLoader(
+    val_dataset, batch_size=batch_size, shuffle=False, collate_fn=od_collate_fn
+)
+
+# 辞書型変数にまとめる
+dataloaders_dict = {"train": train_dataloader, "val": val_dataloader}
+
+# 動作の確認
+batch_iterator = iter(dataloaders_dict["val"])
+images, targets = next(batch_iterator)
+print(images.size())
+print(len(targets))
+print(targets[1].size())
+
+# %%
+print(train_dataset.__len__())
+print(val_dataset.__len__())
+
+# %% パッケージ
+from math import sqrt
+from itertools import product
+
+import pandas as pd
+import torch
+from torch.autograd import Function
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+# %% 35層にわたる，vggモジュールを作成
+def make_vgg():
+    layers = []
+    in_channels = 3
+    
+    # vggモジュールで使用する畳み込み層やマックスプーリングのチャネル数
+    cfg = [64, 64, 'M', 128, 128, 'M', 256, 256,
+           256, 'MC', 512, 512, 512, 'M', 512, 512, 512]
+    
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif v == "MC":
+            # ceilは出力サイズを計算結果に対して切り上げで整数にするモード
+            # デフォルトでは出力サイズを計算結果に対して切り下げで整数にする
+            # floorモード
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    
+    pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+    conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)
+    conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
+    layers += [pool5, conv6,
+               nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
+    return nn.ModuleList(layers)
+
+# 動作確認
+vgg_test = make_vgg()
+print(vgg_test)
+
+# %% 8層にわたる，extrasモジュールを作成
+def make_extras():
+    layers = []
+    in_channels = 1024 # vggモジュールから出力された，extraに入力される画像チャネル
+    
+    # extraモジュールの畳み込み層のチャネル数を設定するコンフィグレーション
+    cfg = [256, 512, 128, 256, 128, 256, 128, 256]
+    
+    layers += [nn.Conv2d(in_channels, cfg[0], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[0], cfg[1], kernel_size=(3), stride=2, padding=1)]
+    layers += [nn.Conv2d(cfg[1], cfg[2], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[2], cfg[3], kernel_size=(3), stride=2, padding=1)]
+    layers += [nn.Conv2d(cfg[3], cfg[4], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[4], cfg[5], kernel_size=(3))]
+    layers += [nn.Conv2d(cfg[5], cfg[6], kernel_size=(1))]
+    layers += [nn.Conv2d(cfg[6], cfg[7], kernel_size=(3))]
+    
+    return nn.ModuleList(layers)
+
+# 動作確認
+extras_test = make_extras()
+print(extras_test)
+
+# %% デフォルトボックスのオフセットを出力するloc_layers
+# デフォルトボックスに対する各クラスの信頼度confidenceを出力するconf_layersを作成
+
+def make_loc_conf(num_classes=21, bbox_aspect_num=[4, 6, 6, 6, 4, 4]):
+    loc_layers = []
+    conf_layers = []
+    
+    # VGGの22層目，conv4_3(source1)に対する畳み込み層
+    loc_layers += [nn.Conv2d(512, bbox_aspect_num[0] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(512, bbox_aspect_num[0] * num_classes, kernel_size=3, padding=1)]
+    
+    # VGGの最終層(source2)に対する畳み込み層
+    loc_layers += [nn.Conv2d(1024, bbox_aspect_num[1] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(1024, bbox_aspect_num[1] * num_classes, kernel_size=3, padding=1)]
+    
+    # extra(source3)に対する畳み込み層
+    loc_layers += [nn.Conv2d(512, bbox_aspect_num[2] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(512, bbox_aspect_num[2] * num_classes, kernel_size=3, padding=1)]
+    
+    # extra(source4)に対する畳み込み層
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[3] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(256, bbox_aspect_num[3] * num_classes, kernel_size=3, padding=1)]
+
+    # extra(source5)に対する畳み込み層
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[4] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(256, bbox_aspect_num[4] * num_classes, kernel_size=3, padding=1)]
+    
+    # extra(source6)に対する畳み込み層
+    loc_layers += [nn.Conv2d(256, bbox_aspect_num[5] * 4, kernel_size=3, padding=1)]
+    conf_layers += [nn.Conv2d(256, bbox_aspect_num[5] * num_classes, kernel_size=3, padding=1)]
+    
+    return nn.ModuleList(loc_layers), nn.ModuleList(conf_layers)
+
+# 動作確認
+loc_test, conf_test = make_loc_conf()
+print(loc_test)
+print(conf_test)
+
+# %%
